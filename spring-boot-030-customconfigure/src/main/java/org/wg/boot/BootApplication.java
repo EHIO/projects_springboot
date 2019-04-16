ee87aecb108ea31f07e7ae832f173e196d85f414
Run <1160061218@qq.com>
2019-04-17 02:31:56 +0800

    rearrangement

--- spring-boot-030-customconfigure/src/main/java/org/wg/boot/BootApplication.java
+++ spring-boot-030-customconfigure/src/main/java/org/wg/boot/BootApplication.java
@@ -1,12 +1,13 @@
 package org.wg.boot;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoot02Application {
+public class BootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot02Application.class, args);
+		SpringApplication.run(BootApplication.class, args);
 	}
 }
